8b86e866b3f31bb60ede6ef1a3a74a5f21c93e5b
jeonghyun <k557330@naver.com>
2021-04-14 18:30:31 +0900

    pad pro horizontal layout

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 import "../stylesheets/VideoListContainer.scss";
 
 const VideoList = () => {
-    return <section className="video-list-container">VideoList</section>;
+    return <section className="video-list-container"></section>;
 };
 
 export default VideoList;
