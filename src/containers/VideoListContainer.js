dabcc62e1233308bf8ab6df06b4b5501d88f3e15
jeonghyun <k557330@naver.com>
2021-04-15 19:43:41 +0900

    pad pro layout

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -3,7 +3,17 @@ import React from "react";
 import "../stylesheets/VideoListContainer.scss";
 
 const VideoList = () => {
-    return <section className="video-list-container"></section>;
+    return (
+        <>
+            <section className="video-list-container"></section>
+            <section className="video-list-sidebar-container">
+                <input type="checkbox" id="video-list" />
+                <label htmlFor="video-list">Newly</label>
+                <div className="video-list-sidebar"></div>
+                <label htmlFor="video-list" className="dim"></label>
+            </section>
+        </>
+    );
 };
 
 export default VideoList;
