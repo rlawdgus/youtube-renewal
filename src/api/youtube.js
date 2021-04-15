84beef23a6bf1109f077687ec6de13b74d164ae3
jeonghyun <k557330@naver.com>
2021-04-15 20:18:05 +0900

    youtube api

---
+++ src/api/youtube.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const URL = "https://www.googleapis.com/youtube/v3";
+const KEY = "AIzaSyCshtUyMgrQ3QjhfQfHOeigO_sZdG_GBlk";
+
+export const requestGetVideoList = async () => {
+    const config = {
+        params: {
+            key: KEY,
+            part: "id,snippet,contentDetails,statistics,player",
+            chart: "mostPopular",
+            maxResults: 1,
+        },
+    };
+
+    const response = await axios.get(`${URL}/videos`, config);
+
+    return response.data;
+};
--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+
+import { requestGetVideoList } from "../api/youtube";
 
 import "../stylesheets/VideoListContainer.scss";
 
 const VideoList = () => {
+    const [videoList, setVideoList] = useState([]);
+    const getVideoList = useCallback(async () => {
+        setVideoList(await requestGetVideoList());
+    }, []);
+    useEffect(() => {
+        getVideoList();
+    }, []);
+
     return (
         <>
+            {console.log(videoList)}
             <section className="video-list-container"></section>
             <section className="video-list-sidebar-container">
                 <input type="checkbox" id="video-list" />
